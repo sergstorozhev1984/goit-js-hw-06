9a919c5fb277157b0a3c9733c1f1604289b18d82
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2022-11-10 02:13:34 +0200

    add 4 task

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+let counterValue = 0;
+const count = document.querySelector('#value');
+
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const onBtnIncrementClick = () => {
+    return count.textContent = counterValue++;
+}
+btnIncrement.addEventListener("click", onBtnIncrementClick);
+
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const onBtnDecrementClick = () => {
+    return count.textContent = counterValue--;
+}
+btnDecrement.addEventListener("click", onBtnDecrementClick);
+
+
+
+
+ 
