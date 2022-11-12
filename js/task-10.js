ddb20b475b1bf5d2ab5df30f5d928619e032aa72
Sergey <92057970+sergstorozhev1984@users.noreply.github.com>
2022-11-12 19:43:08 +0200

    all 10 tasks

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
+const refs = {
+  inputEl: document.querySelector('input'),
+  buttonCreateEl: document.querySelector('[data-create]'),
+  buttonDestroyEl: document.querySelector('[data-destroy]'),
+  divBoxesEl: document.querySelector('#boxes'),
+}
+refs.buttonCreateEl.addEventListener('click', getAmount);
+refs.buttonDestroyEl.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+  let amount = +refs.inputEl.value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i+=1) {
+    refs.divBoxesEl.innerHTML += `<div style="width:${size + i * 10}px; height:${size + i * 10}px; background-color: ${getRandomHexColor()}"></div>`;
+  }
+  refs.divBoxesEl;
+}
+
+function destroyBoxes() {
+  refs.divBoxesEl.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
